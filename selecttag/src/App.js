757addc9e994ad712d3e2cab49f2506df4337d49
ayushi <ayushi8855@gmail.com>
2022-09-11 22:35:53 +0530

    done

--- selecttag/src/App.js
+++ selecttag/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CustomSelect from './CustomSelect';
 
+const styles={
+  app:{
+    backgroundColor:'rgba(0,0,0,0.1)',
+    justifyItems:'center',
+    alignItems:'center',
+    display:'grid',
+    height:'100vh',
+    fontFamily:'Arial',
+    color:'rgba(0,0,100,1)',
+    fontSize:25,
+   
+  },
+  select:{
+    width:'100%',
+    maxWidth:600,
+   
+  }
+}
+
+const options=[
+  {label:'Red',value:'red'},
+  {label:'Blue',value:'blue'},
+  {label:'Green',value:'green'},
+  {label:'Yellow',value:'yellow'},
+]
+
+function onChangeInput(value){
+  console.log(value);
+}
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.app}>
+      <CustomSelect isMulti={true} style={styles.select} defaultValue={[options[3]]} onChange={onChangeInput} options={options} label="Choose a color" />
     </div>
+  
   );
+
+  
 }
 
 export default App;
